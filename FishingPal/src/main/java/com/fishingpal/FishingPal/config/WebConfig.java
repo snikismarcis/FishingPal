1822f2a3595ca8db2eed2d42b8f75b7f3802f21b
snikismarcis <marcis.snikis@edu.rtu.lv>
2026-03-21 14:35:12 +0200

    removed duplicate of cors config

--- FishingPal/src/main/java/com/fishingpal/FishingPal/config/WebConfig.java
+++ FishingPal/src/main/java/com/fishingpal/FishingPal/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.fishingpal.FishingPal.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,15 +8,6 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://fishing-pal.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
